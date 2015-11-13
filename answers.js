89a0741076fe56cdc80105066aab70d3c8bbaf47
bakoliasn <bakoliasn@gmail.com>
2015-11-13 18:44:29 +0000

    added "last" function to funtctions branch

--- answers.js
+++ answers.js
@@ -1,7 +1,15 @@
-//input string return first char 
+//input string log first char 
 function initial (inputString){
     console.log(inputString[0]);
 }
 initial("Nick");
 initial("hello");
 initial("");
+
+//input string log last char
+function last (inputString){
+    console.log(inputString[(inputString.length - 1)]);
+}
+last("nick");
+last("");
+
