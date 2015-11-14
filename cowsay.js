587bdafad61228b8969f5eb7c5f9f00f144cb906
bakoliasn <bakoliasn@gmail.com>
2015-11-14 20:50:00 +0000

    created the cowsay function in the-cow-whisperer branch. added an if so that if the text is longer than 30 characters it will print on a new line (only at the " ".

---
+++ cowsay.js
@@ -0,0 +1,30 @@
+function cowsay(text){
+    var counter = 0;
+    var temp= "hello";
+    if(text.length > 30){    
+        for (var i = 30; i > 0; i--){    
+            if (text[i]===" " && counter < 1){
+                temp = text.slice(i);
+                counter++;
+            }
+        }
+        console.log(temp);
+    console.log("\\\ " + text + " /");
+    console.log("\\\ " + temp + " /");
+    console.log("    ^__^   ");
+    console.log(" \\\  (oo)\\\_______");
+    console.log("    (__)\\\       )\\\/\\\ ");
+    console.log("        ||----w | ");
+    console.log("        ||     || ");
+    
+    }else{
+    console.log("\\\ " + text + " /");
+    console.log("    ^__^   ");
+    console.log(" \\\  (oo)\\\_______");
+    console.log("    (__)\\\       )\\\/\\\ ");
+    console.log("        ||----w | ");
+    console.log("        ||     || ");
+}
+    
+}
+cowsay("hello this should be a string of over 30 chars i hope but i didn't count"); 
