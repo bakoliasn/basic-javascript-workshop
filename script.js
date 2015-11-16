1901c241b67e9d5b22d53ec6148f6c329f474a4a
bakoliasn <bakoliasn@gmail.com>
2015-11-16 16:01:58 +0000

    just fixed up some of the text + returned array of wrong guesses.

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
         if(arr[i]===number){
             return alert("You've got the right number! You win... NOTHING!!!");
         }else if(i===3){ 
-            return alert("You did not guess the right number... It was: " + number);
+            return alert("You did not guess the right number... \nIt was: " + number + "\nyou guessed: " + arr);
         }else {
             alert("wrong number try again you have " + (3 - i) + " tries left!");
         }
